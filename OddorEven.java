f3de60cdef3af67107e0e8aa0bcdd6f895cb8fa8
Praveena-Balaji <126135225+Praveena-Balaji@users.noreply.github.com>
2023-02-23 11:26:33 +0530

    Add files via upload

---
+++ OddorEven.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class OddorEven {
+
+	public static void main(String[] args) {
+		int a = 1990;
+		if(a%2==0) {
+			System.out.println("a is an even number");
+			
+		}
+		else {
+			System.out.println("a is an odd number");
+		}
+
+	}
+
+}
