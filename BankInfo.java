f1f746240815837e350f59478f45542e2e980645
Praveena-Balaji <126135225+Praveena-Balaji@users.noreply.github.com>
2023-03-06 21:38:32 +0530

    Add files via upload

---
+++ BankInfo.java
@@ -0,0 +1,18 @@
+package week3.day1;
+
+public class BankInfo {
+		public void saving() {
+			System.out.println("Have savings account in the bank");
+		}
+		public void fixed() {
+			System.out.println("Have a fixed amount in the bank for future");
+		}
+		public void deposit() {
+			System.out.println("The depositted amount is one lakh rupees");
+		}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
